1d58e0fb8364df71c5c2959fad40d92bb91143d1
Abraham Moreno <adiaz2821@gmail.com>
2019-10-01 16:12:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,12 +58,12 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-programmer_hash
+programmer_hash = {
  [:yukihiro_matsumoto => {
    :known_for => "Ruby",
     :languages => ["LISP", "C"]
  }]
-
+}
 	programmer_hash = 
  		{
         :grace_hopper => {
